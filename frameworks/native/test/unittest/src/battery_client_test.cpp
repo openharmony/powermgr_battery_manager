9a503cba3fb9d281ac2212aadcc9de006bddb565
ShiJie <shijie20@huawei.com>
2022-02-23 01:13:31 -0800

    test: Optimize use cases
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: Icdbc1bd07eabaaff352f8f8be1af72ca2d69de64

--- frameworks/native/test/unittest/src/battery_client_test.cpp
+++ frameworks/native/test/unittest/src/battery_client_test.cpp
@@ -13,48 +13,45 @@
  * limitations under the License.
  */
 
-#include <csignal>
-#include <iostream>
-#include <chrono>
-#include <condition_variable>
-#include <fstream>
-#include <memory>
-#include <mutex>
-#include <streambuf>
-#include <string>
-#include <thread>
-#include <vector>
-#include <sys/stat.h>
-
-#include "battery_log.h"
-#include "iservice_registry.h"
-#include "if_system_ability_manager.h"
-#include "system_ability_definition.h"
-#include "ipc_skeleton.h"
-#include "string_ex.h"
-#include "sys_param.h"
-
-#include "battery_srv_client.h"
-#include "battery_service.h"
 #include "battery_client_test.h"
+
+#include <iostream>
+#include <string>
+#include "if_system_ability_manager.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
+#include "battery_log.h"
+#include "battery_service.h"
+#include "battery_srv_client.h"
+#include "test_utils.h"
 
 using namespace testing::ext;
 using namespace OHOS::PowerMgr;
 using namespace OHOS;
 using namespace std;
 
+namespace {
+bool g_isMock = false;
 static sptr<BatteryService> g_service;
+}
 
 void BatteryClientTest::SetUpTestCase(void)
 {
     g_service = DelayedSpSingleton<BatteryService>::GetInstance();
     g_service->OnStart();
+    auto& client = BatterySrvClient::GetInstance();
+    GTEST_LOG_(INFO) << "is mock: " << client.GetPresent() << " g_isMock: " << g_isMock;
+    if (!client.GetPresent()) {
+        g_isMock = true;
+        TestUtils::InitTest();
+        g_service->ChangePath("/data/local/tmp");
+    }
 }
 
 void BatteryClientTest::TearDownTestCase(void)
 {
-    g_service->OnStop();
-    DelayedSpSingleton<BatteryService>::DestroyInstance();
+    g_isMock = false;
+    TestUtils::ResetOnline();
 }
 
 void BatteryClientTest::SetUp(void)
@@ -66,34 +63,6 @@ void BatteryClientTest::TearDown(void)
 }
 
 namespace {
-std::string CreateFile(std::string path, std::string content)
-{
-    std::ofstream stream(path.c_str());
-    if (!stream.is_open()) {
-        BATTERY_HILOGI(LABEL_TEST, "Cannot create file %{public}s", path.c_str());
-        return nullptr;
-    }
-    stream << content.c_str() << std::endl;
-    stream.close();
-    return path;
-}
-
-void MockFileInit()
-{
-    std::string path = "/data/local/tmp";
-    mkdir("/data/local/tmp/battery", S_IRWXU);
-    mkdir("/data/local/tmp/ohos_charger", S_IRWXU);
-    mkdir("/data/local/tmp/ohos-fgu", S_IRWXU);
-    BATTERY_HILOGI(LABEL_TEST, "MockFileInit enter.");
-    sleep(1);
-
-    CreateFile("/data/local/tmp/battery/online", "1");
-    CreateFile("/data/local/tmp/battery/type", "Battery");
-    CreateFile("/data/local/tmp/ohos_charger/health", "Unknown");
-    CreateFile("/data/local/tmp/ohos-fgu/temp", "345");
-    g_service->ChangePath(path);
-}
-
 /**
  * @tc.name: BatteryClient001
  * @tc.desc: Test IBatterySrv interface GetCapacity
@@ -102,15 +71,21 @@ void MockFileInit()
 HWTEST_F (BatteryClientTest, BatteryClient001, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient001 start.");
-    MockFileInit();
-    CreateFile("/data/local/tmp/battery/capacity", "22");
 
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto capacity = BatterySrvClient.GetCapacity();
-    BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::capacity=%{public}d", capacity);
-    GTEST_LOG_(INFO) << "BatteryClient::BatteryClient001 executing, capacity=" << capacity;
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/capacity", "22");
+        auto capacity = BatterySrvClient.GetCapacity();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::capacity=%{public}d", capacity);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient001 executing, capacity=" << capacity;
+        ASSERT_TRUE(capacity == 22);
+    } else {
+        auto capacity = BatterySrvClient.GetCapacity();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::capacity=%{public}d", capacity);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient001 executing, capacity=" << capacity;
+        ASSERT_TRUE(capacity <= 100 && capacity >= 0);
+    }
 
-    ASSERT_TRUE(capacity == 22);
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient001 end.");
 }
 
@@ -121,14 +96,20 @@ HWTEST_F (BatteryClientTest, BatteryClient001, TestSize.Level1)
  */
 HWTEST_F (BatteryClientTest, BatteryClient002, TestSize.Level1)
 {
-    BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient002 start.");
-    CreateFile("/data/local/tmp/battery/status", "Not charging");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto chargeState = BatterySrvClient.GetChargingStatus();
-    GTEST_LOG_(INFO) << "BatteryClient::BatteryClient002 executing, chargeState=" << int(chargeState);
-
-    ASSERT_TRUE(chargeState == OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_DISABLE);
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/status", "Not charging");
+        auto chargeState = BatterySrvClient.GetChargingStatus();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::chargeState=%{public}d", int(chargeState));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient002 executing, chargeState=" << int(chargeState);
+        ASSERT_TRUE(chargeState == OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_DISABLE);
+    } else {
+        auto chargeState = BatterySrvClient.GetChargingStatus();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::chargeState=%{public}d", int(chargeState));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient002 executing, chargeState=" << int(chargeState);
+        ASSERT_TRUE(chargeState == OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_ENABLE ||
+            chargeState == OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_FULL);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient002 end.");
 }
 
@@ -140,13 +121,19 @@ HWTEST_F (BatteryClientTest, BatteryClient002, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient003, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient003 start.");
-    CreateFile("/data/local/tmp/battery/health", "Cold");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto healthState = BatterySrvClient.GetHealthStatus();
-    GTEST_LOG_(INFO) << "BatteryClient::BatteryClient003 executing, healthState=" << int(healthState);
-
-    ASSERT_TRUE(healthState == OHOS::PowerMgr::BatteryHealthState(4));
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/health", "Cold");
+        auto healthState = BatterySrvClient.GetHealthStatus();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::healthState=%{public}d", int(healthState));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient003 executing, healthState=" << int(healthState);
+        ASSERT_TRUE(healthState == OHOS::PowerMgr::BatteryHealthState::HEALTH_STATE_COLD);
+    } else {
+        auto healthState = BatterySrvClient.GetHealthStatus();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::healthState=%{public}d", int(healthState));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient003 executing, healthState=" << int(healthState);
+        ASSERT_TRUE(healthState == OHOS::PowerMgr::BatteryHealthState::HEALTH_STATE_GOOD);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient003 end.");
 }
 
@@ -158,13 +145,19 @@ HWTEST_F (BatteryClientTest, BatteryClient003, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient004, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient004 start.");
-    CreateFile("/data/local/tmp/battery/present", "0");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto present = BatterySrvClient.GetPresent();
-    GTEST_LOG_(INFO) << "BatteryClient::BatteryClient004 executing, present=" << present;
-
-    ASSERT_FALSE(present);
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/present", "0");
+        auto present = BatterySrvClient.GetPresent();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::present=%{public}d", present);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient004 executing, present=" << present;
+        ASSERT_FALSE(present);
+    } else {
+        auto present = BatterySrvClient.GetPresent();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::present=%{public}d", present);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient004 executing, present=" << present;
+        ASSERT_TRUE(present);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient004 end.");
 }
 
@@ -176,14 +169,20 @@ HWTEST_F (BatteryClientTest, BatteryClient004, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient005, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient005 start.");
-    CreateFile("/data/local/tmp/battery/voltage_avg", "4654321");
-    CreateFile("/data/local/tmp/battery/voltage_now", "4654321");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto voltage = BatterySrvClient.GetVoltage();
-    BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::voltage=%{public}d", voltage);
-
-    ASSERT_TRUE(voltage == 4654321);
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/voltage_avg", "4654321");
+        TestUtils::WriteMock("/data/local/tmp/battery/voltage_now", "4654321");
+        auto voltage = BatterySrvClient.GetVoltage();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::voltage=%{public}d", voltage);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient005 executing, voltage=" << voltage;
+        ASSERT_TRUE(voltage == 4654321);
+    } else {
+        auto voltage = BatterySrvClient.GetVoltage();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::voltage=%{public}d", voltage);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient005 executing, voltage=" << voltage;
+        ASSERT_TRUE(voltage >= 0);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient005 end.");
 }
 
@@ -195,13 +194,19 @@ HWTEST_F (BatteryClientTest, BatteryClient005, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient006, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient006 start.");
-    CreateFile("/data/local/tmp/battery/temp", "222");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto temperature = BatterySrvClient.GetBatteryTemperature();
-    BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::voltage=%{public}d", temperature);
-
-    ASSERT_TRUE(temperature == 222);
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/battery/temp", "222");
+        auto temperature = BatterySrvClient.GetBatteryTemperature();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::temperature=%{public}d", temperature);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient006 executing, temperature=" << temperature;
+        ASSERT_TRUE(temperature == 222);
+    } else {
+        auto temperature = BatterySrvClient.GetBatteryTemperature();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::temperature=%{public}d", temperature);
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient006 executing, temperature=" << temperature;
+        ASSERT_TRUE(temperature >= 0 && temperature <= 600);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient006 end.");
 }
 
@@ -213,13 +218,19 @@ HWTEST_F (BatteryClientTest, BatteryClient006, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient007, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient007 start.");
-    CreateFile("/data/local/tmp/ohos-fgu/technology", "H2");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto technology = BatterySrvClient.GetTechnology();
-    BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::technology=%{public}s", technology.c_str());
-
-    ASSERT_TRUE(technology == "H2");
+    if (g_isMock) {
+        TestUtils::WriteMock("/data/local/tmp/ohos-fgu/technology", "H2");
+        auto technology = BatterySrvClient.GetTechnology();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::technology=%{public}s", technology.c_str());
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient007 executing, technology=" << technology;
+        ASSERT_TRUE(technology == "H2");
+    } else {
+        auto technology = BatterySrvClient.GetTechnology();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::technology=%{public}s", technology.c_str());
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient007 executing, technology=" << technology;
+        ASSERT_TRUE(technology == "Li-poly");
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient007 end.");
 }
 
@@ -231,16 +242,21 @@ HWTEST_F (BatteryClientTest, BatteryClient007, TestSize.Level1)
 HWTEST_F (BatteryClientTest, BatteryClient008, TestSize.Level1)
 {
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient008 start.");
-    CreateFile("/data/local/tmp/ohos_charger/online", "1");
-    CreateFile("/data/local/tmp/ohos_charger/type", "USB");
-    CreateFile("/data/local/tmp/battery/type", "USB");
-    CreateFile("/data/local/tmp/ohos-fgu/type", "USB");
-
     auto& BatterySrvClient = BatterySrvClient::GetInstance();
-    auto pluggedType = BatterySrvClient.GetPluggedType();
-    BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::pluggedType=%{public}d", pluggedType);
-
-    ASSERT_TRUE(pluggedType == OHOS::PowerMgr::BatteryPluggedType::PLUGGED_TYPE_USB);
+    if (g_isMock) {
+        TestUtils::ResetOnline();
+        TestUtils::WriteMock("/data/local/tmp/ohos_charger/online", "1");
+        TestUtils::WriteMock("/data/local/tmp/ohos_charger/type", "USB");
+        auto pluggedType = BatterySrvClient.GetPluggedType();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::pluggedType=%{public}d", int(pluggedType));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient008 executing, pluggedType=" << int(pluggedType);
+        ASSERT_TRUE(pluggedType == OHOS::PowerMgr::BatteryPluggedType::PLUGGED_TYPE_USB);
+    } else {
+        auto pluggedType = BatterySrvClient.GetPluggedType();
+        BATTERY_HILOGI(LABEL_TEST, "BatteryClientTest::pluggedType=%{public}d", int(pluggedType));
+        GTEST_LOG_(INFO) << "BatteryClient::BatteryClient008 executing, pluggedType=" << int(pluggedType);
+        ASSERT_TRUE(pluggedType == OHOS::PowerMgr::BatteryPluggedType::PLUGGED_TYPE_USB);
+    }
     BATTERY_HILOGD(LABEL_TEST, "BatteryClient::BatteryClient008 end.");
 }
 }
---
+++ utils/native/include/test_utils.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef POWER_TEST_UTILS_H
+#define POWER_TEST_UTILS_H
+
+#include <string>
+
+namespace OHOS {
+namespace PowerMgr {
+class TestUtils {
+public:
+    static void WriteMock(const std::string& path, const std::string content);
+    static void InitTest();
+    static void ResetOnline();
+};
+} // namespace PowerMgr
+} // namespace OHOS
+
+#endif // POWER_TEST_UTILS_H
