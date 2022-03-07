acc6f7eff08e73d597a732b12f332264c0892157
toutes <zhaoruiyuan2@huawei.com>
2022-03-07 06:50:43 -0800

    Add syscap SystemCapability.PowerManager.BatteryManager.Extension
    Signed-off-by: toutes <zhaoruiyuan2@huawei.com>
    Change-Id: I794aeb80c2b4aac147c51b940fc5a62ec4920afd

--- interfaces/kits/js/@ohos.batteryinfo.d.ts
+++ interfaces/kits/js/@ohos.batteryinfo.d.ts
@@ -19,7 +19,7 @@
  * <p>Battery information includes the remaining battery power,
  * voltage, temperature, model, and charger type.
  *
- * @syscap SystemCapability.PowerManager.BatteryManage.Core
+ * @syscap SystemCapability.PowerManager.BatteryManager.Core
  * @since 6
  */
 declare namespace batteryInfo {
@@ -74,7 +74,7 @@ declare namespace batteryInfo {
     /**
      * Indicates the charger type of a device.
      *
-     * @syscap SystemCapability.PowerManager.BatteryManage.Core
+     * @syscap SystemCapability.PowerManager.BatteryManager.Core
      * @since 6
      */
     export enum BatteryPluggedType {
@@ -103,7 +103,7 @@ declare namespace batteryInfo {
     /**
      * Indicates the battery charging status of a device.
      *
-     * @syscap SystemCapability.PowerManager.BatteryManage.Core
+     * @syscap SystemCapability.PowerManager.BatteryManager.Core
      * @since 6
      */
     export enum BatteryChargeState {
@@ -132,7 +132,7 @@ declare namespace batteryInfo {
     /**
      * Indicates the battery health status of a device.
      *
-     * @syscap SystemCapability.PowerManager.BatteryManage.Core
+     * @syscap SystemCapability.PowerManager.BatteryManager.Core
      * @since 6
      */
     export enum BatteryHealthState {
