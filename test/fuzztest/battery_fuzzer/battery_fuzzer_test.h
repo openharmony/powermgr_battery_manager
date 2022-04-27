afeb5c51862f1cc6c54111896b091a298cdc3b2c
zhaoruiyuan <zhaoruiyuan2@huawei.com>
2022-04-27 11:26:49 +0800

    feat: add FUZZ test
    Signed-off-by: zhaoruiyuan <zhaoruiyuan2@huawei.com>

---
+++ test/fuzztest/battery_fuzzer/battery_fuzzer_test.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* This files contains faultlog config modules. */
+
+#ifndef BATTERY_FUZZER_TEST_H
+#define BATTERY_FUZZER_TEST_H
+
+#define FUZZ_PROJECT_NAME "battery_fuzzer"
+
+enum class ApiNumber {
+    NUM_ZERO = 0,
+    NUM_ONE,
+    NUM_TWO,
+    NUM_THREE,
+    NUM_FOUR,
+    NUM_FIVE,
+    NUM_SIX,
+    NUM_SEVEN
+};
+
+#endif
