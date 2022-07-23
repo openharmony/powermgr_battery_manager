96daa0e776b41ca8c5db0cd194434c59530372ed
ShiJie <shijie20@huawei.com>
2022-07-23 14:40:57 +0800

    fix: CHARGING and DISCHARGING event modification
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: I3eef65b018f4db51e59e8c6e97800428be328380

--- services/native/src/battery_service_subscriber.cpp
+++ services/native/src/battery_service_subscriber.cpp
@@ -296,8 +296,7 @@ bool BatteryServiceSubscriber::HandleBatteryChargingEvent(const BatteryInfo& inf
     publishInfo.SetOrdered(false);
     bool isSuccess = true;
 
-    if (static_cast<uint32_t>(info.GetChargeState()) !=
-        static_cast<uint32_t>(BatteryChargeState::CHARGE_STATE_ENABLE)) {
+    if (info.GetChargeState() != BatteryChargeState::CHARGE_STATE_ENABLE) {
         g_batteryChargingOnce = false;
         return isSuccess;
     }
@@ -329,7 +328,7 @@ bool BatteryServiceSubscriber::HandleBatteryDischargingEvent(const BatteryInfo& 
     publishInfo.SetOrdered(false);
     bool isSuccess = true;
 
-    if (static_cast<uint32_t>(info.GetChargeState()) != static_cast<uint32_t>(BatteryChargeState::CHARGE_STATE_NONE)) {
+    if (info.GetChargeState() == BatteryChargeState::CHARGE_STATE_ENABLE) {
         g_batteryDischargingOnce = false;
         return isSuccess;
     }
