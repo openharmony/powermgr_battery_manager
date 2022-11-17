7db4bcee24774d4641bb0f60d532d8b370cb7a45
yangziyong <nsyangziyong@huawei.com>
2022-11-17 11:01:34 +0800

    test: add test case for code coverage optimization
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>

--- services/native/test/unittest/src/battery_light_test.cpp
+++ services/native/test/unittest/src/battery_light_test.cpp
@@ -14,11 +14,11 @@
  */
 
 #include "battery_light_test.h"
-#include <memory>
 #include "battery_config.h"
 #include "battery_config_test.h"
 #include "battery_light.h"
 #include "power_common.h"
+#include <memory>
 
 using namespace testing::ext;
 
@@ -28,104 +28,157 @@ namespace {
 constexpr const char* SYSTEM_BATTERY_CONFIG_PATH = "/system/etc/battery/battery_config.json";
 
 BatteryLight g_light;
-}
+} // namespace
+
 void BatteryLightTest::SetUpTestCase()
 {
     BatteryConfig::GetInstance().ParseConfig(SYSTEM_BATTERY_CONFIG_PATH);
     g_light.InitLight();
-
-    GTEST_LOG_(INFO) << "available battery light " << g_light.isAvailable();
+    g_light.TurnOff();
 }
 
-void BatteryLightTest::TearDown(void)
+void BatteryLightTest::TearDown()
 {
     g_light.TurnOff();
 }
 
 /**
  * @tc.name: BatteryLight001
- * @tc.desc: ChargingStatus is Discharging, Not bright lights
+ * @tc.desc: Turn off light
  * @tc.type: FUNC
+ * @tc.require: issueI5YZR1
  */
-HWTEST_F (BatteryLightTest, BatteryLight001, TestSize.Level1)
+HWTEST_F(BatteryLightTest, BatteryLight001, TestSize.Level1)
 {
-    RETURN_IF(!g_light.isAvailable());
-    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
-    // First turn on the light
-    ASSERT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 11));
-    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::YELLOW_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::YELLOW_LIGHT);
-
-    ASSERT_FALSE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_NONE, 0));
-    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::LIGHT_OFF;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    g_light.TurnOff();
+    EXPECT_TRUE(g_light.GetLightColor() == 0U);
 }
 
 /**
  * @tc.name: BatteryLight002
- * @tc.desc: ChargingStatus is Charging, capacity is 9, the red light on
+ * @tc.desc: Turn on light
  * @tc.type: FUNC
+ * @tc.require: issueI5YZR1
  */
-HWTEST_F (BatteryLightTest, BatteryLight002, TestSize.Level1)
+HWTEST_F(BatteryLightTest, BatteryLight002, TestSize.Level1)
 {
-    RETURN_IF(!g_light.isAvailable());
-    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
-    ASSERT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 9));
-    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::RED_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::RED_LIGHT);
+    uint32_t color = 0U;
+    int32_t brightness = 1;
+    g_light.TurnOn(color, brightness);
+    EXPECT_TRUE(g_light.GetLightColor() == color);
 }
 
 /**
  * @tc.name: BatteryLight003
- * @tc.desc: ChargingStatus is Charging, capacity is 89, the yellow light on
+ * @tc.desc: Update light color according to the capacity value 1
  * @tc.type: FUNC
+ * @tc.require: issueI5YZR1
  */
-HWTEST_F (BatteryLightTest, BatteryLight003, TestSize.Level1)
+HWTEST_F(BatteryLightTest, BatteryLight003, TestSize.Level1)
 {
-    RETURN_IF(!g_light.isAvailable());
-    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
-    ASSERT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 89));
-    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::YELLOW_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::YELLOW_LIGHT);
+    int32_t capacity = 1;
+    g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, capacity);
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::RED_LIGHT);
 }
 
 /**
  * @tc.name: BatteryLight004
- * @tc.desc: ChargingStatus is Charging, capacity is 100, the green light on
+ * @tc.desc: Turn on light set light color
  * @tc.type: FUNC
+ * @tc.require: issueI5YZR1
  */
-HWTEST_F (BatteryLightTest, BatteryLight004, TestSize.Level1)
+HWTEST_F(BatteryLightTest, BatteryLight004, TestSize.Level1)
 {
-    RETURN_IF(!g_light.isAvailable());
-    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
-    ASSERT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 100));
-    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::GREEN_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
+    uint32_t color = 0x7fffffff;
+    int32_t brightness = 0x7fffffff;
+    g_light.TurnOn(color, brightness);
+    EXPECT_TRUE(g_light.GetLightColor() == color);
 }
 
 /**
  * @tc.name: BatteryLight005
- * @tc.desc: ChargingStatus is Charging, capacity is -1, Light does not change
+ * @tc.desc: ChargingStatus is Discharging, Not bright lights
  * @tc.type: FUNC
  */
-HWTEST_F (BatteryLightTest, BatteryLight005, TestSize.Level1)
+HWTEST_F(BatteryLightTest, BatteryLight005, TestSize.Level1)
 {
     RETURN_IF(!g_light.isAvailable());
     GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
     // First turn on the light
-    ASSERT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 91));
+    EXPECT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 11));
+    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::YELLOW_LIGHT;
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::YELLOW_LIGHT);
+
+    EXPECT_FALSE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_NONE, 0));
+    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::LIGHT_OFF;
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+}
+
+/**
+ * @tc.name: BatteryLight006
+ * @tc.desc: ChargingStatus is Charging, capacity is 9, the red light on
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryLightTest, BatteryLight006, TestSize.Level1)
+{
+    RETURN_IF(!g_light.isAvailable());
+    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    EXPECT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 9));
+    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::RED_LIGHT;
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::RED_LIGHT);
+}
+
+/**
+ * @tc.name: BatteryLight007
+ * @tc.desc: ChargingStatus is Charging, capacity is 89, the yellow light on
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryLightTest, BatteryLight007, TestSize.Level1)
+{
+    RETURN_IF(!g_light.isAvailable());
+    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    EXPECT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 89));
+    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::YELLOW_LIGHT;
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::YELLOW_LIGHT);
+}
+
+/**
+ * @tc.name: BatteryLight008
+ * @tc.desc: ChargingStatus is Charging, capacity is 100, the green light on
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryLightTest, BatteryLight008, TestSize.Level1)
+{
+    RETURN_IF(!g_light.isAvailable());
+    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    EXPECT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 100));
     GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::GREEN_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
+}
+
+/**
+ * @tc.name: BatteryLight009
+ * @tc.desc: ChargingStatus is Charging, capacity is -1, Light does not change
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryLightTest, BatteryLight009, TestSize.Level1)
+{
+    RETURN_IF(!g_light.isAvailable());
+    GTEST_LOG_(INFO) << "initial:" << g_light.GetLightColor();
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::LIGHT_OFF);
+    // First turn on the light
+    EXPECT_TRUE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, 91));
+    GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::GREEN_LIGHT;
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
 
     // Capacity invalid value, Not bright lights
-    ASSERT_FALSE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, -1));
+    EXPECT_FALSE(g_light.UpdateColor(BatteryChargeState::CHARGE_STATE_ENABLE, -1));
     GTEST_LOG_(INFO) << "actual:" << g_light.GetLightColor() << "=expect:" << BatteryConfigTest::GREEN_LIGHT;
-    ASSERT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
+    EXPECT_EQ(g_light.GetLightColor(), BatteryConfigTest::GREEN_LIGHT);
 }
 } // namespace PowerMgr
 } // namespace OHOS
---
+++ services/native/test/unittest/src/battery_srv_proxy_mock_test.cpp
@@ -0,0 +1,223 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "battery_srv_proxy_mock_test.h"
+#include "battery_srv_proxy.h"
+#include "ipc_object_stub.h"
+#include "mock_remote_object.h"
+#include <hisysevent.h>
+
+using namespace testing::ext;
+using namespace OHOS::HiviewDFX;
+using namespace OHOS::PowerMgr;
+using namespace OHOS;
+using namespace std;
+
+std::shared_ptr<BatterySrvProxy> g_proxy;
+sptr<IRemoteObject> remoteObj;
+
+void BatteryProxyMockTest::SetUpTestCase()
+{
+#ifdef ENABLE_REMOTE_INTERFACE
+    remoteObj = new MockRemoteObject();
+#else
+    remoteObj = new IPCObjectStub();
+#endif
+    g_proxy = std::make_shared<BatterySrvProxy>(remoteObj);
+}
+
+void BatteryProxyMockTest::TearDownTestCase()
+{
+    remoteObj = nullptr;
+    g_proxy = nullptr;
+}
+
+void BatteryProxyMockTest::SetUp() {}
+
+void BatteryProxyMockTest::TearDown() {}
+
+namespace {
+/**
+ * @tc.name: BatteryProxyMockTest_001
+ * @tc.desc: test BatterySrvProxy::GetCapacity() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_001, TestSize.Level0)
+{
+    int32_t capacity = g_proxy->GetCapacity();
+    EXPECT_TRUE(capacity == INVALID_BATT_INT_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_002
+ * @tc.desc: test BatterySrvProxy::GetChargingStatus() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_002, TestSize.Level0)
+{
+    BatteryChargeState chargeState = g_proxy->GetChargingStatus();
+    EXPECT_TRUE(chargeState >= BatteryChargeState::CHARGE_STATE_NONE &&
+        chargeState <= BatteryChargeState::CHARGE_STATE_BUTT); // the enum value range of BatteryChargeState
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_003
+ * @tc.desc: test BatterySrvProxy::GetHealthStatus() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_003, TestSize.Level0)
+{
+    BatteryHealthState healthState = g_proxy->GetHealthStatus();
+    EXPECT_TRUE(healthState >= BatteryHealthState::HEALTH_STATE_UNKNOWN &&
+        healthState <= BatteryHealthState::HEALTH_STATE_BUTT); // the enum value range of BatteryHealthState
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_004
+ * @tc.desc: test BatterySrvProxy::GetPluggedType() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_004, TestSize.Level0)
+{
+    BatteryPluggedType pluggedType = g_proxy->GetPluggedType();
+    EXPECT_TRUE(pluggedType >= BatteryPluggedType::PLUGGED_TYPE_NONE &&
+        pluggedType <= BatteryPluggedType::PLUGGED_TYPE_BUTT); // the enum value range of BatteryPluggedType
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_005
+ * @tc.desc: test BatterySrvProxy::GetPresent() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_005, TestSize.Level0)
+{
+    bool isPresent = g_proxy->GetPresent();
+    EXPECT_TRUE(isPresent == INVALID_BATT_BOOL_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_006
+ * @tc.desc: test BatterySrvProxy::GetTchnology() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_006, TestSize.Level0)
+{
+    std::string technology = g_proxy->GetTechnology();
+    EXPECT_TRUE(technology == INVALID_STRING_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_007
+ * @tc.desc: test BatterySrvProxy::GetTotalEnergy() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_007, TestSize.Level0)
+{
+    int32_t totalEnergy = g_proxy->GetTotalEnergy();
+    EXPECT_TRUE(totalEnergy == INVALID_BATT_INT_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_008
+ * @tc.desc: test BatterySrvProxy::GetCurrentAverage() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_008, TestSize.Level0)
+{
+    int32_t currentAverage = g_proxy->GetCurrentAverage();
+    EXPECT_TRUE(currentAverage == INVALID_BATT_INT_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_009
+ * @tc.desc: test BatterySrvProxy::GetNowCurrent() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_009, TestSize.Level0)
+{
+    int32_t nowCurrent = g_proxy->GetNowCurrent();
+    EXPECT_TRUE(nowCurrent == INVALID_BATT_INT_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_010
+ * @tc.desc: test BatterySrvProxy::GetRemainEnergy() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_010, TestSize.Level0)
+{
+    int32_t remainEnergy = g_proxy->GetRemainEnergy();
+    EXPECT_TRUE(remainEnergy == INVALID_BATT_INT_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_011
+ * @tc.desc: test BatterySrvProxy::GetBatteryTemperature() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_011, TestSize.Level0)
+{
+    int32_t temperature = g_proxy->GetBatteryTemperature();
+    EXPECT_TRUE(temperature == INVALID_BATT_TEMP_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_012
+ * @tc.desc: test BatterySrvProxy::GetBatteryLevel() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_012, TestSize.Level0)
+{
+    BatteryLevel batteryLevel = g_proxy->GetBatteryLevel();
+    EXPECT_TRUE(batteryLevel >= BatteryLevel::LEVEL_NONE &&
+        batteryLevel <= BatteryLevel::LEVEL_RESERVED); // the enum value range of BatteryLevel
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_013
+ * @tc.desc: test BatterySrvProxy::GetRemainingChargeTime() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_013, TestSize.Level0)
+{
+    int64_t remainChargeTime = g_proxy->GetRemainingChargeTime();
+    EXPECT_TRUE(remainChargeTime == INVALID_REMAINING_CHARGE_TIME_VALUE);
+}
+
+/**
+ * @tc.name: BatteryProxyMockTest_014
+ * @tc.desc: test BatterySrvProxy::GetVoltage() when an exception is raised
+ * @tc.type: FUNC
+ * @tc.require: issueI5X13X
+ */
+HWTEST_F(BatteryProxyMockTest, BatteryProxyMockTest_014, TestSize.Level0)
+{
+    int32_t voltage = g_proxy->GetVoltage();
+    EXPECT_TRUE(voltage == INVALID_BATT_INT_VALUE);
+}
+} // namespace
