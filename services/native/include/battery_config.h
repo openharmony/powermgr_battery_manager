8b9fb18d04624be94131823e2a411bb8592b593b
yuanbo <yuanbo@huawei.com>
2023-03-15 14:10:55 +0800

    feat: battery support config policy
    Signed-off-by: yuanbo <yuanbo@huawei.com>

--- services/native/include/battery_config.h
+++ services/native/include/battery_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -35,7 +35,7 @@ public:
         uint32_t rgb;
     };
     static BatteryConfig& GetInstance();
-    bool ParseConfig(std::string configPath = "");
+    bool ParseConfig();
     bool IsExist(std::string key) const;
     int32_t GetInt(std::string key, int32_t defVal = 0) const;
     const std::vector<LightConf>& GetLightConf() const;
