7bd59010b4d56d52fa8275fe3e9b1de647c89ddf
ShiJie <shijie20@huawei.com>
2023-05-25 22:16:40 +0800

    test: add sutb fuzz
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: I13959742169f331807a29a3dc76a79ec55a3d856

--- test/fuzztest/battery_fuzzer/battery_fuzzer_test.cpp
+++ test/fuzztest/battery_fuzzer/battery_fuzzer_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,150 +17,138 @@
 
 #include "battery_fuzzer_test.h"
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <random>
-#include <algorithm>
+
 #include "battery_info.h"
+#include "battery_service.h"
 #include "battery_srv_client.h"
+#include "message_parcel.h"
+#include "securec.h"
 
 using namespace OHOS::PowerMgr;
+using namespace OHOS;
 using namespace std;
 
 namespace {
 auto& g_batterySrvClient = BatterySrvClient::GetInstance();
-}
+const int32_t REWIND_READ_DATA = 0;
+} // namespace
 
-static void TestGetCapacity(const uint8_t* data)
+static void TestGetCapacity([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetCapacity();
 }
 
-static void TestGetVoltage(const uint8_t* data)
+static void TestGetVoltage([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetVoltage();
 }
 
-static void TestGetChargingStatus(const uint8_t* data)
+static void TestGetChargingStatus([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetChargingStatus();
 }
 
-static void TestGetHealthStatus(const uint8_t* data)
+static void TestGetHealthStatus([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetHealthStatus();
 }
 
-static void TestGetPluggedType(const uint8_t* data)
+static void TestGetPluggedType([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetPluggedType();
 }
 
-static void TestGetPresent(const uint8_t* data)
+static void TestGetPresent([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetPresent();
 }
 
-static void TestGetTechnology(const uint8_t* data)
+static void TestGetTechnology([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
-        return;
-    }
-
     g_batterySrvClient.GetTechnology();
 }
 
-static void TestGetTemperature(const uint8_t* data)
+static void TestGetTemperature([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    if (data == nullptr) {
+    g_batterySrvClient.GetBatteryTemperature();
+}
+
+static void TestGetNowCurrent([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
+{
+    g_batterySrvClient.GetNowCurrent();
+}
+
+static void TestGetRemainEnergy([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
+{
+    g_batterySrvClient.GetRemainEnergy();
+}
+
+static void TestGetTotalEnergy([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
+{
+    g_batterySrvClient.GetTotalEnergy();
+}
+
+static void TestGetCapacityLevel([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
+{
+    g_batterySrvClient.GetCapacityLevel();
+}
+
+static void TestGetRemainingChargeTime([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
+{
+    g_batterySrvClient.GetRemainingChargeTime();
+}
+
+static void TestBatteryServiceStub(const uint8_t* data, size_t size)
+{
+    uint32_t code;
+    if (size < sizeof(code)) {
+        return;
+    }
+    if (memcpy_s(&code, sizeof(code), data, sizeof(code)) != EOK) {
         return;
     }
 
-    g_batterySrvClient.GetBatteryTemperature();
+    MessageParcel datas;
+    datas.WriteInterfaceToken(BatteryService::GetDescriptor());
+    datas.WriteBuffer(data, size);
+    datas.RewindRead(REWIND_READ_DATA);
+    MessageParcel reply;
+    MessageOption option;
+    DelayedSpSingleton<BatteryService>::GetInstance()->OnRemoteRequest(code, datas, reply, option);
 }
+
+static std::vector<std::function<void(const uint8_t*, size_t)>> fuzzFunc = {
+    &TestGetCapacity,
+    &TestGetVoltage,
+    &TestGetChargingStatus,
+    &TestGetHealthStatus,
+    &TestGetPluggedType,
+    &TestGetPresent,
+    &TestGetTechnology,
+    &TestGetTemperature,
+    &TestGetNowCurrent,
+    &TestGetRemainEnergy,
+    &TestGetTotalEnergy,
+    &TestGetCapacityLevel,
+    &TestGetRemainingChargeTime
+};
 
 namespace OHOS {
 bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
 {
-    int32_t idSize = 8;
-    if (static_cast<int32_t>(size) > idSize) {
-        std::random_device rd;
-        std::default_random_engine engine(rd());
-        std::uniform_int_distribution<int32_t> randomNum(static_cast<int32_t>(ApiNumber::NUM_ZERO),
-            static_cast<int32_t>(ApiNumber::NUM_END) - 1);
-        int32_t number = randomNum(engine);
-
-        switch (static_cast<ApiNumber>(number)) {
-            case ApiNumber::NUM_ZERO:
-                TestGetCapacity(data);
-                break;
-            case ApiNumber::NUM_ONE:
-                TestGetVoltage(data);
-                break;
-            case ApiNumber::NUM_TWO:
-                TestGetChargingStatus(data);
-                break;
-            case ApiNumber::NUM_THREE:
-                TestGetHealthStatus(data);
-                break;
-            case ApiNumber::NUM_FOUR:
-                TestGetPluggedType(data);
-                break;
-            case ApiNumber::NUM_FIVE:
-                TestGetPresent(data);
-                break;
-            case ApiNumber::NUM_SIX:
-                TestGetTechnology(data);
-                break;
-            case ApiNumber::NUM_SEVEN:
-                TestGetTemperature(data);
-                break;
-            case ApiNumber::NUM_EIGHT:
-                g_batterySrvClient.GetNowCurrent();
-                break;
-            case ApiNumber::NUM_NINE:
-                g_batterySrvClient.GetRemainEnergy();
-                break;
-            case ApiNumber::NUM_TEN:
-                g_batterySrvClient.GetTotalEnergy();
-                break;
-            case ApiNumber::NUM_ELEVEN:
-                g_batterySrvClient.GetCapacityLevel();
-                break;
-            case ApiNumber::NUM_TWELVE:
-                g_batterySrvClient.GetRemainingChargeTime();
-                break;
-            default:
-                break;
-        }
-    }
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::uniform_int_distribution<int32_t> randomNum(0, fuzzFunc.size() - 1);
+    int32_t number = randomNum(engine);
+    fuzzFunc[number](data, size);
+    TestBatteryServiceStub(data, size);
     return true;
 }
-}
+} // namespace OHOS
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
