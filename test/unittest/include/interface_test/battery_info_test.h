7993f1152fa5909383a03437dcf05e658dd099be
chenjunwu <chenjunwu4@huawei.com>
2024-08-20 21:02:20 +0800

    fix: Optimize the directory structure of test cases && add new test cases
    Signed-off-by: chenjunwu <chenjunwu4@huawei.com>

--- test/unittest/include/interface_test/battery_info_test.h
+++ test/unittest/include/interface_test/battery_info_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,19 +13,18 @@
  * limitations under the License.
  */
 
-#ifndef BATTERY_SERVICE_TEST_H
-#define BATTERY_SERVICE_TEST_H
+#ifndef BATTERY_INFO_TEST_H
+#define BATTERY_INFO_TEST_H
 
 #include <gtest/gtest.h>
 
 namespace OHOS {
 namespace PowerMgr {
-class BatteryServiceTest : public testing::Test {
+class BatteryInfoTest : public testing::Test {
 public:
+    static void SetUpTestCase();
     static void TearDownTestCase();
-    int32_t ReadFile(const char* path, char* buf, size_t size);
-    int32_t WriteFile(const char* path, char* buf, size_t size);
 };
 } // namespace PowerMgr
 } // namespace OHOS
-#endif // BATTERY_SERVICE_TEST_H
+#endif // BATTERY_INFO_TEST_H
---
+++ test/unittest/src/battery_event_test.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "battery_event_test.h"
+#ifdef GTEST
+#define private   public
+#define protected public
+#endif
+
+#include <iostream>
+#include <string>
+
+#include "common_event_data.h"
+#include "common_event_manager.h"
+#include "common_event_publish_info.h"
+#include "common_event_support.h"
+#include "battery_notify.h"
+#include "battery_log.h"
+#include "test_utils.h"
+
+using namespace testing::ext;
+using namespace OHOS::PowerMgr;
+using namespace OHOS;
+using namespace std;
+
+namespace {
+shared_ptr<BatteryNotify> g_batteryNotify = nullptr;
+} // namespace
+
+void BatteryEventTest::SetUpTestCase()
+{
+    if (g_batteryNotify == nullptr) {
+        g_batteryNotify = make_shared<BatteryNotify>();
+    }
+}
+
+void BatteryEventTest::TearDownTestCase()
+{
+    if (g_batteryNotify != nullptr) {
+        g_batteryNotify.reset();
+        g_batteryNotify = nullptr;
+    }
+}
+/**
+ * @tc.name: BatteryEventTest001
+ * @tc.desc: test PublishLowEvent function
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryEventTest, BatteryEventTest001, TestSize.Level1)
+{
+    BATTERY_HILOGD(LABEL_TEST, "BatteryEventTest001 start.");
+    BatteryInfo info;
+    bool ret = g_batteryNotify->PublishLowEvent(info);
+    EXPECT_TRUE(ret);
+    BATTERY_HILOGD(LABEL_TEST, "BatteryEventTest001 end.");
+}
+
+/**
+ * @tc.name: BatteryEventTest002
+ * @tc.desc: test PublishCustomEvent function
+ * @tc.type: FUNC
+ */
+HWTEST_F(BatteryEventTest, BatteryEventTest002, TestSize.Level1)
+{
+    BATTERY_HILOGD(LABEL_TEST, "BatteryEventTest002 start.");
+    BatteryInfo info;
+    bool ret = g_batteryNotify->PublishCustomEvent(info, "test.battery.custom.event");
+    EXPECT_TRUE(ret);
+    BATTERY_HILOGD(LABEL_TEST, "BatteryEventTest002 end.");
+}
