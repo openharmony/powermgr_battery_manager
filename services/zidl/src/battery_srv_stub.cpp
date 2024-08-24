7b17fbf6a9f59a70ed23b3de157b11d32acb1d79
hxf233333 <huxiaofei11@huawei.com>
2024-08-24 22:18:07 +0800

    fix: modify delay time
    Signed-off-by: hxf233333 <huxiaofei11@huawei.com>

--- services/zidl/src/battery_srv_stub.cpp
+++ services/zidl/src/battery_srv_stub.cpp
@@ -39,7 +39,7 @@ int BatterySrvStub::OnRemoteRequest(uint32_t code, MessageParcel& data, MessageP
         return E_GET_POWER_SERVICE_FAILED;
     }
 
-    const int DFX_DELAY_S = 10;
+    const int DFX_DELAY_S = 60;
     int id = HiviewDFX::XCollie::GetInstance().SetTimer("BatteryManagerCallbackStub", DFX_DELAY_S, nullptr, nullptr,
         HiviewDFX::XCOLLIE_FLAG_LOG);
     int32_t ret = CheckRequestCode(code, data, reply, option);
