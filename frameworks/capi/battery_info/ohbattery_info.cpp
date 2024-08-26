a8e129a94a947a4b5e46e8fb1bac1650d983511c
z30053694 <zhangchen190@huawei.com>
2024-08-26 20:03:21 +0800

    feat: added capi
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I62bfed80db321c50c371f72acc68e6dd0560425c
    
    feat: capi step 2
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I4cd550314e2722bb8cca0006eb3d8a5bfa690ed0
    
    feat: cpi step2
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: Ifbae6a736301a53add4b1f4672570836fbf11de1
    
    fix: added ndk target
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: Iebb896a0711659c8e5a4525a81942fee93380fa3
    
    fix: gn format
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I0dc44628e38c90064e78bf05603bbbd19bb30fb1
    
    fix: identified the output name of ndk
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: Ica2f0b63723008079344ae06490970f41d2379ca
    
    fix: added comment, removed the executable for test
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I170b0b188fa7673bd77c513308d90b18c1482a58
    
    fix: changed @since to version 13
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I4ff93b5a631d29710e4edfb60cd7573e0eb52a41
    
    fix: added comment
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I8756e559bea16b9c7bd59d3b8fea27056d39238f
    
    fix: removed logs
    
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I58be4a797382843acf097f98634d19fec31888e7

---
+++ frameworks/capi/battery_info/ohbattery_info.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ohbattery_info.h"
+
+#include  <cinttypes>
+
+#include "battery_srv_client.h"
+
+using namespace OHOS::PowerMgr;
+
+int32_t OH_BatteryInfo_GetCapacity()
+{
+    BatterySrvClient& batterySrvClient = BatterySrvClient::GetInstance();
+    int32_t ret = batterySrvClient.GetCapacity();
+    return ret;
+}
+
+BatteryInfo_BatteryPluggedType OH_BatteryInfo_GetPluggedType()
+{
+    BatterySrvClient& batterySrvClient = BatterySrvClient::GetInstance();
+    uint32_t result = static_cast<uint32_t>(batterySrvClient.GetPluggedType());
+    BatteryInfo_BatteryPluggedType ret = PLUGGED_TYPE_NONE;
+    switch (result) {
+        case static_cast<uint32_t>(PLUGGED_TYPE_AC):
+            ret = PLUGGED_TYPE_AC;
+            break;
+        case static_cast<uint32_t>(PLUGGED_TYPE_USB):
+            ret = PLUGGED_TYPE_USB;
+            break;
+        case static_cast<uint32_t>(PLUGGED_TYPE_WIRELESS):
+            ret = PLUGGED_TYPE_WIRELESS;
+            break;
+        case static_cast<uint32_t>(PLUGGED_TYPE_NONE):
+            ret = PLUGGED_TYPE_NONE;
+            break;
+        default:
+            ret = PLUGGED_TYPE_BUTT;
+            break;
+    }
+    return ret;
+}
