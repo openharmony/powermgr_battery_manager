1ed0378ab56588061dc16cec2d1d069d72941dac
chenjunwu <chenjunwu4@huawei.com>
2024-09-21 14:29:19 +0800

    fix: add IsNativePermissionGranted permission check （cherry picked commit from <gitee.com//openharmony/powermgr_battery_manager/commit/9720e7448355fdf2fed948dfbc42f6c2526579e9> Signed-off-by: cjw123qq <chenjunwu4@huawei.com>

--- services/native/src/battery_service.cpp
+++ services/native/src/battery_service.cpp
@@ -450,7 +450,7 @@ void BatteryService::SetLowCapacityThreshold()
 
 BatteryError BatteryService::SetBatteryConfig(const std::string& sceneName, const std::string& value)
 {
-    if (!Permission::IsSystem()) {
+    if (!Permission::IsSystem() || !Permission::IsNativePermissionGranted("ohos.permission.POWER_OPTIMIZATION")) {
         BATTERY_HILOGI(FEATURE_BATT_INFO, "SetBatteryConfig failed, System permission intercept");
         return BatteryError::ERR_SYSTEM_API_DENIED;
     }
