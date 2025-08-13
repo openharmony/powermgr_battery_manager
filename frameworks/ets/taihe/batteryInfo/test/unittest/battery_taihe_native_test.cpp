c60440c328af1520e3c3c367888b6a9074909985
cjw123qq <chenjunwu4@huawei.com>
2025-08-13 16:30:13 +0800

    fix: add enum default value
    Signed-off-by: cjw123qq <chenjunwu4@huawei.com>

--- frameworks/ets/taihe/batteryInfo/test/unittest/battery_taihe_native_test.cpp
+++ frameworks/ets/taihe/batteryInfo/test/unittest/battery_taihe_native_test.cpp
@@ -34,6 +34,10 @@ using namespace testing::ext;
 namespace {
 BatteryError g_returnError = BatteryError::ERR_OK;
 uint32_t g_count = 0;
+OHOS::PowerMgr::BatteryChargeState g_chargeState = OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_BUTT;
+OHOS::PowerMgr::BatteryHealthState g_healthState = OHOS::PowerMgr::BatteryHealthState::HEALTH_STATE_BUTT;
+OHOS::PowerMgr::BatteryPluggedType g_pluggedType = OHOS::PowerMgr::BatteryPluggedType::PLUGGED_TYPE_BUTT;
+OHOS::PowerMgr::BatteryCapacityLevel g_capacityLevel = OHOS::PowerMgr::BatteryCapacityLevel::LEVEL_NONE;
 }
 
 namespace taihe {
@@ -69,19 +73,19 @@ int32_t BatterySrvClient::GetCapacity()
 BatteryChargeState BatterySrvClient::GetChargingStatus()
 {
     ++g_count;
-    return BatteryChargeState::CHARGE_STATE_BUTT;
+    return g_chargeState;
 }
 
 BatteryHealthState BatterySrvClient::GetHealthStatus()
 {
     ++g_count;
-    return BatteryHealthState::HEALTH_STATE_BUTT;
+    return g_healthState;
 }
 
 BatteryPluggedType BatterySrvClient::GetPluggedType()
 {
     ++g_count;
-    return BatteryPluggedType::PLUGGED_TYPE_BUTT;
+    return g_pluggedType;
 }
 
 int32_t BatterySrvClient::GetVoltage()
@@ -129,7 +133,7 @@ int32_t BatterySrvClient::GetTotalEnergy()
 BatteryCapacityLevel BatterySrvClient::GetCapacityLevel()
 {
     ++g_count;
-    return BatteryCapacityLevel::LEVEL_NONE;
+    return g_capacityLevel;
 }
 
 int64_t BatterySrvClient::GetRemainingChargeTime()
@@ -149,6 +153,10 @@ public:
     {
         g_returnError = BatteryError::ERR_OK;
         g_count = 0;
+        g_chargeState = OHOS::PowerMgr::BatteryChargeState::CHARGE_STATE_BUTT;
+        g_healthState = OHOS::PowerMgr::BatteryHealthState::HEALTH_STATE_BUTT;
+        g_pluggedType = OHOS::PowerMgr::BatteryPluggedType::PLUGGED_TYPE_BUTT;
+        g_capacityLevel = OHOS::PowerMgr::BatteryCapacityLevel::LEVEL_NONE;
     }
 };
 
@@ -246,5 +254,97 @@ HWTEST_F(BatteryTaiheNativeTest, BatteryTaiheNativeTest_004, TestSize.Level1)
     RemainingEnergy();
     EXPECT_EQ(g_count, 13); // func call counts
     BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_004 end");
+}
+
+/**
+ * @tc.name: BatteryTaiheNativeTest_005
+ * @tc.desc: test battery taihe native ChargingStatus
+ * @tc.type: FUNC
+ * @tc.require: issue#ICSS7U
+ */
+HWTEST_F(BatteryTaiheNativeTest, BatteryTaiheNativeTest_005, TestSize.Level1)
+{
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_005 start");
+    using OHOS::PowerMgr::BatteryChargeState;
+    for (int i = static_cast<int>(BatteryChargeState::CHARGE_STATE_NONE);
+        i <= static_cast<int>(BatteryChargeState::CHARGE_STATE_BUTT); ++i) {
+        g_chargeState = BatteryChargeState(i);
+        ohos::batteryInfo::BatteryChargeState testChargeState = ChargingStatus();
+        if (g_chargeState == BatteryChargeState::CHARGE_STATE_BUTT) {
+            EXPECT_EQ(testChargeState, ohos::batteryInfo::BatteryChargeState::from_value(0));
+        } else {
+            EXPECT_EQ(testChargeState, ohos::batteryInfo::BatteryChargeState::from_value(i));
+        }
+    }
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_005 end");
+}
+
+/**
+ * @tc.name: BatteryTaiheNativeTest_006
+ * @tc.desc: test battery taihe native HealthStatus
+ * @tc.type: FUNC
+ * @tc.require: issue#ICSS7U
+ */
+HWTEST_F(BatteryTaiheNativeTest, BatteryTaiheNativeTest_006, TestSize.Level1)
+{
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_006 start");
+    using OHOS::PowerMgr::BatteryHealthState;
+    for (int i = static_cast<int>(BatteryHealthState::HEALTH_STATE_UNKNOWN);
+        i <= static_cast<int>(BatteryHealthState::HEALTH_STATE_BUTT); ++i) {
+        g_healthState = BatteryHealthState(i);
+        ohos::batteryInfo::BatteryHealthState testHealthState = HealthStatus();
+        if (g_healthState == BatteryHealthState::HEALTH_STATE_BUTT) {
+            EXPECT_EQ(testHealthState, ohos::batteryInfo::BatteryHealthState::from_value(0));
+        } else {
+            EXPECT_EQ(testHealthState, ohos::batteryInfo::BatteryHealthState::from_value(i));
+        }
+    }
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_006 end");
+}
+
+/**
+ * @tc.name: BatteryTaiheNativeTest_007
+ * @tc.desc: test battery taihe native PluggedType
+ * @tc.type: FUNC
+ * @tc.require: issue#ICSS7U
+ */
+HWTEST_F(BatteryTaiheNativeTest, BatteryTaiheNativeTest_007, TestSize.Level1)
+{
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_007 start");
+    using OHOS::PowerMgr::BatteryPluggedType;
+    for (int i = static_cast<int>(BatteryPluggedType::PLUGGED_TYPE_NONE);
+        i <= static_cast<int>(BatteryPluggedType::PLUGGED_TYPE_BUTT); ++i) {
+        g_pluggedType = BatteryPluggedType(i);
+        ohos::batteryInfo::BatteryPluggedType testPluggedType = PluggedType();
+        if (g_pluggedType == BatteryPluggedType::PLUGGED_TYPE_BUTT) {
+            EXPECT_EQ(testPluggedType, ohos::batteryInfo::BatteryPluggedType::from_value(0));
+        } else {
+            EXPECT_EQ(testPluggedType, ohos::batteryInfo::BatteryChargeState::from_value(i));
+        }
+    }
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_007 end");
+}
+
+/**
+ * @tc.name: BatteryTaiheNativeTest_008
+ * @tc.desc: test battery taihe native GetCapacityLevel
+ * @tc.type: FUNC
+ * @tc.require: issue#ICSS7U
+ */
+HWTEST_F(BatteryTaiheNativeTest, BatteryTaiheNativeTest_008, TestSize.Level1)
+{
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_008 start");
+    using OHOS::PowerMgr::BatteryCapacityLevel;
+    for (int i = static_cast<int>(BatteryCapacityLevel::LEVEL_NONE);
+        i <= static_cast<int>(BatteryCapacityLevel::LEVEL_RESERVED); ++i) {
+        g_capacityLevel = BatteryCapacityLevel(i);
+        ohos::batteryInfo::BatteryCapacityLevel testCapacityLevel = GetCapacityLevel();
+        if (g_capacityLevel == BatteryCapacityLevel::LEVEL_RESERVED) {
+            EXPECT_EQ(testCapacityLevel, ohos::batteryInfo::BatteryCapacityLevel::from_value(0));
+        } else {
+            EXPECT_EQ(testCapacityLevel, ohos::batteryInfo::BatteryCapacityLevel::from_value(i));
+        }
+    }
+    BATTERY_HILOGI(LABEL_TEST, "BatteryTaiheNativeTest_008 end");
 }
 }
